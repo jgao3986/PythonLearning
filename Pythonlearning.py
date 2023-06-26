21d2fd36d0f2df3214e9f51cab5b5d18222785d8
Junshen Gao <>
2023-06-26 12:58:25 -0400

    Day 1

---
+++ Pythonlearning.py
@@ -0,0 +1,5 @@
+name = input("What is your name? ")
+print("Hello " + name)
+birth_year = input("Enter your birth year: ")
+age = 2023 - int(birth_year)
+print(age)
---
+++ calculator.py
@@ -0,0 +1,4 @@
+first_value = input("First: ")
+second_value = input("Second: ")
+sum = float(first_value) + float(second_value)
+print("Sum: " + str(sum))
